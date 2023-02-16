f5bbe1a4d4532a12889708262b81b3ea627625cf
Zerthox <zerthox@gmail.com>
2023-02-17 00:11:04 +0100

    Update dependencies

--- src/action.rs
+++ src/action.rs
@@ -1,8 +1,9 @@
 use arcdps::imgui::{StyleVar, Ui};
 
 /// An action for ordered lists in UI.
-#[derive(Debug, Clone)]
+#[derive(Debug, Default, Clone)]
 pub enum Action {
+    #[default]
     None,
     Remove(usize),
     Up(usize),
@@ -12,9 +13,10 @@ pub enum Action {
 impl Action {
     /// Renders action buttons.
     pub fn render_buttons(&mut self, ui: &Ui, index: usize, len: usize) {
+        let is_first = index == 0;
+        let is_last = index == len - 1;
         let current_alpha = ui.clone_style().alpha;
 
-        let is_first = index == 0;
         let style = ui.push_style_var(StyleVar::Alpha(if is_first { 0.3 } else { current_alpha }));
         if ui.button(format!("^##{index}")) && !is_first {
             *self = Self::Up(index);
@@ -22,7 +24,6 @@ impl Action {
         style.pop();
 
         ui.same_line();
-        let is_last = index == len - 1;
         let style = ui.push_style_var(StyleVar::Alpha(if is_last { 0.3 } else { current_alpha }));
         if ui.button(format!("v##{index}")) && !is_last {
             *self = Self::Down(index);

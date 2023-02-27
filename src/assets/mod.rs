75f94325f2c741546a1f92fb3777bccacfbefed6
Zerthox <zerthox@gmail.com>
2023-02-27 13:10:14 +0100

    Add error reporting

--- src/assets/mod.rs
+++ src/assets/mod.rs
@@ -4,6 +4,7 @@ use arc_util::ui::{
 };
 use arcdps::d3d11_device;
 use include_img::include_img;
+use log::error;
 use once_cell::sync::Lazy;
 use windows::Win32::Graphics::{
     Direct3D11::D3D11_USAGE_IMMUTABLE, Dxgi::Common::DXGI_FORMAT_R8G8B8A8_UNORM,
@@ -14,8 +15,11 @@ fn init_icon(data: &'static [u8]) -> Option<Icon> {
     let format = DXGI_FORMAT_R8G8B8A8_UNORM;
     let texture =
         create_texture2d_from_mem(device, data, 32, 32, 32 * 4, format, D3D11_USAGE_IMMUTABLE)
+            .map_err(|err| error!("failed to create texture: {err}"))
             .ok()?;
-    create_texture2d_view(device, &texture, format).ok()
+    create_texture2d_view(device, &texture, format)
+        .map_err(|err| error!("failed to create texture view: {err}"))
+        .ok()
 }
 
 pub static FOOD_ICON: Lazy<Option<Icon>> =
